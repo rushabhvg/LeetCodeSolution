6cec33a2d17a10f6033dad63356572e2ec78af78
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2022-12-24 12:38:51 +0530

    Create MiddleOfTheLinkedList.cpp

---
+++ 0876. Middle of the Linked List/MiddleOfTheLinkedList.cpp
@@ -0,0 +1,56 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode *ptr=head;
+        int cnt=0;
+        while(ptr!=NULL) {
+            cnt+=1;
+            ptr=ptr->next;
+        }
+
+        // if(cnt%2==0) {
+        //     cnt/=2;
+        // } else {
+        //     cnt /=2 ;
+        // }
+        cnt/=2;
+        ptr=head;
+        while(cnt>0) {
+            ptr=ptr->next;
+            cnt--;
+        }
+        return ptr;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
