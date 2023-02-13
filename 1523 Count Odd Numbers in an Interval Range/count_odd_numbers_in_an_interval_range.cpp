0fd861002e12f6dab79fcf81bf349222a0c7dcbe
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-02-13 08:37:52 +0530

    Create count_odd_numbers_in_an_interval_range.cpp

---
+++ 1523 Count Odd Numbers in an Interval Range/count_odd_numbers_in_an_interval_range.cpp
@@ -0,0 +1,30 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        if(low%2==1) --low;
+        if(high%2==1) ++high;
+        return (high-low)/2;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
