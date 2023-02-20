e06c4dc074039e3f294d7fa29f885ced59abebd3
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-02-20 07:55:57 +0530

    Create guess_number_higher_or_lower.cpp

---
+++ 0374 Guess Number Higher or Lower/guess_number_higher_or_lower.cpp
@@ -0,0 +1,51 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+class Solution {
+public:
+    int guessNumber(int n) {
+        int start = 0;
+        int last = n;
+        int mid;
+        while(start<=last) {
+            mid = start + (last-start)/2;
+            int ans = guess(mid);
+            if(ans==-1) {
+                last = mid-1;
+            } else if (ans==1) {
+                start = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        return last+1;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
