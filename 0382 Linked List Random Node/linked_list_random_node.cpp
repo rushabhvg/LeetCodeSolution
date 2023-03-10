2060ba173efdeb120523424a5445700c424260a2
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-03-10 17:16:56 +0530

    Create linked_list_random_node.cpp

---
+++ 0382 Linked List Random Node/linked_list_random_node.cpp
@@ -0,0 +1,59 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *temp;
+    ListNode *curr;
+    Solution(ListNode* head) {
+        temp = head;
+        curr = head;
+    }
+    int getRandom() {
+        int cnt = 1, chosenValue = temp->val;
+        curr = temp;
+        while(curr != nullptr) {
+            if(rand()%cnt==cnt-1) {
+                chosenValue = curr->val;
+            }
+            cnt+=1;
+            curr = curr->next;
+        }
+        return chosenValue;
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(head);
+ * int param_1 = obj->getRandom();
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
