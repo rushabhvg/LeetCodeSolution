e2d17456acbce3e1446b9d01765c0b183bca0bb8
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-04-01 22:56:10 +0530

    Create binary_search.cpp

---
+++ 0704 Binary Search/binary_search.cpp
@@ -0,0 +1,39 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = int(nums.size()) - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;   
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
