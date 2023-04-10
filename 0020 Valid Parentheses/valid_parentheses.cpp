7605795e21a271f537098c3e67dbe9b22648bb08
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-04-10 18:14:16 +0530

    Create valid_parentheses.cpp

---
+++ 0020 Valid Parentheses/valid_parentheses.cpp
@@ -0,0 +1,47 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        for (char c : s) {
+            if (c == '(' || c == '{' || c == '[') {
+                stk.push(c);
+            } else {
+                if (stk.empty()) {
+                    return false;
+                }
+                if (c == ')' && stk.top() == '(') {
+                    stk.pop();
+                } else if (c == '}' && stk.top() == '{') {
+                    stk.pop();
+                } else if (c == ']' && stk.top() == '[') {
+                    stk.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stk.empty();
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
