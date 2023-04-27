ba93546f718edba0ec686bd3195e9d2a7d4fe41b
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-04-27 08:45:37 +0530

    Create bulb_switcher.cpp

---
+++ 0319 Bulb Switcher/bulb_switcher.cpp
@@ -0,0 +1,28 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n);
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
