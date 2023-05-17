4d9ad426b202a3e34f79b2b65882160923177762
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-05-17 11:17:16 +0530

    Create promise_pool.js

---
+++ 2636 Promise Pool/promise_pool.js
@@ -0,0 +1,37 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+    const evaluateNext = () => functions[n++]?.().then(evaluateNext);
+    return Promise.all(functions.slice(0, n).map(f => f().then(evaluateNext)));
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
