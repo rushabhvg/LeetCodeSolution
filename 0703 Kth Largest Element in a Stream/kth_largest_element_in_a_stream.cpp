db9f0d4285fe32d42c90bb0481bf2199f4ac43b0
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-05-23 23:03:55 +0530

    Create kth_largest_element_in_a_stream.cpp

---
+++ 0703 Kth Largest Element in a Stream/kth_largest_element_in_a_stream.cpp
@@ -0,0 +1,47 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class KthLargest {
+public:
+    priority_queue<int,vector<int>,greater<int>> pq;
+    int sz;
+    KthLargest(int k, vector<int>& nums) {
+        sz = k;
+        for(int i=0;i<nums.size();i++){
+            pq.push(nums[i]);
+        }
+    }
+    
+    int add(int val) {
+       pq.push(val);
+        while(pq.size()>sz){
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
