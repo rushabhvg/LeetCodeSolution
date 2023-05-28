2597a80d83ad42c416f4346cf7ab93d617a22cce
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-05-28 09:53:14 +0530

    Create maximum_strictly_iIncreasing_cells_in_a_matrix.cpp

---
+++ 2713 Maximum Strictly Increasing Cells in a Matrix/maximum_strictly_iIncreasing_cells_in_a_matrix.cpp
@@ -0,0 +1,48 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+  int maxIncreasingCells(vector<vector<int>>& mat) {
+    int m = mat.size(), n = mat[0].size();
+    vector<int> r(m), c(n);
+    unordered_map<int, vector<vector<int>>> vmap;
+    vector<vector<int>> temp(m, vector<int>(n));
+    set<int> s;
+    for(int i =0; i<m; i++) {
+      for(int j=0; j<n; j++) {
+        vmap[-mat[i][j]].push_back({i, j});
+        s.insert(-mat[i][j]);
+      }
+    }
+    for(int x: s) {
+      for(auto& v: vmap[x]) {
+        temp[v[0]][v[1]] = max( r[v[0]], c[v[1]] )+1;
+      }
+      for(auto v: vmap[x]) {
+        r[v[0]] = max(r[v[0]], temp[v[0]][v[1]]);
+        c[v[1]] = max(c[v[1]], temp[v[0]][v[1]]);
+      }
+    }
+    return max( *max_element(  r.begin(), r.end()  ), *max_element(  c.begin(), c.end()  ) );
+  }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
