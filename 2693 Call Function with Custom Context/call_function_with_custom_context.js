dbdbc4c20727a4fc239741c65bc143051ef7453c
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-05-30 21:14:43 +0530

    Create call_function_with_custom_context.js

---
+++ 2693 Call Function with Custom Context/call_function_with_custom_context.js
@@ -0,0 +1,40 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+  Object.defineProperty(context, 'fn', {
+    value: this,
+    enumerable: false,
+  });
+
+  return context.fn(...args);
+}
+
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
