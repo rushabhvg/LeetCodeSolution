fceb6fda458d627113175a7188497fb9f6342e3e
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-06-03 09:44:48 +0530

    Create nested_array_generator.js

---
+++ 2649 Nested Array Generator/nested_array_generator.js
@@ -0,0 +1,43 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * @param {Array} arr
+ * @return {Generator}
+ */
+var inorderTraversal = function* (arr) {
+    if(!Array.isArray(arr)) {
+        yield arr
+        return
+    }
+
+    for(let i = 0; i < arr.length; i++) {
+        yield* inorderTraversal(arr[i])
+    }
+};
+
+/**
+ * const gen = inorderTraversal([1, [2, 3]]);
+ * gen.next().value; // 1
+ * gen.next().value; // 2
+ * gen.next().value; // 3
+ */
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
