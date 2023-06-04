dd0f732354b3fee099ccb6b69a480840e998c68e
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-06-04 17:47:42 +0530

    Create single_number.cpp

---
+++ 0136 Single Number/single_number.cpp
@@ -0,0 +1,30 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xorQ = 0;
+        for(auto &a:nums) xorQ = xorQ ^ a;
+        return xorQ;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
