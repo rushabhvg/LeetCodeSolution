1f0b56d15155fcc0b6376b4ecb4266c603bf2422
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-06-21 11:47:25 +0530

    Create minimum_cost_to_make_array_equal.cpp

---
+++ 2448 Minimum Cost to Make Array Equal/minimum_cost_to_make_array_equal.cpp
@@ -0,0 +1,44 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    long long getCost(vector<int>& nums, vector<int>& cost, int base) {
+        long long result=0;
+        for(int i=0; i<nums.size(); i++) result+= 1L*abs(nums[i]-base)*cost[i];
+        return result;
+    }
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        long long ans=getCost(nums, cost, nums[0]);
+        int left=*min_element(nums.begin(), nums.end());
+        int right=*max_element(nums.begin(), nums.end());\
+        while(left<right) {
+            int mid = (left+right)/2;
+            long long cost1=getCost(nums, cost, mid);
+            long long cost2=getCost(nums, cost, mid+1);
+            ans = min(cost1, cost2);
+            if(cost1>cost2) left=mid+1;
+            else right=mid;
+        }
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
