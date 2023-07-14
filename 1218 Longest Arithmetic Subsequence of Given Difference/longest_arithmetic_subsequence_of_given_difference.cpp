a0593313901ec675e01a74cce641390091ea5353
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-07-14 18:25:35 +0530

    Update longest_arithmetic_subsequence_of_given_difference.cpp

--- 1218 Longest Arithmetic Subsequence of Given Difference/longest_arithmetic_subsequence_of_given_difference.cpp
+++ 1218 Longest Arithmetic Subsequence of Given Difference/longest_arithmetic_subsequence_of_given_difference.cpp
@@ -16,7 +16,7 @@ public:
         int ans=1;
         for(int a: arr) {
             int beforeA = dp.count(a-difference) ? dp[a-difference] : 0;
-            dp[a] = beforeA+Q1;
+            dp[a] = beforeA+1;
             ans = max(ans, dp[a]);
         }
         return ans;
