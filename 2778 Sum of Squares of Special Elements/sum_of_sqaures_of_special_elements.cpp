d9e2dda962252e3c33078610506238770ae45f69
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-07-16 09:50:55 +0530

    Create sum_of_sqaures_of_special_elements.cpp

---
+++ 2778 Sum of Squares of Special Elements/sum_of_sqaures_of_special_elements.cpp
@@ -0,0 +1,33 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int ans=0;
+        int n = nums.size();
+        for(int i=0; i<n; i++) {
+            if( n % (i+1) == 0) ans += nums[i]*nums[i];
+        }
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
