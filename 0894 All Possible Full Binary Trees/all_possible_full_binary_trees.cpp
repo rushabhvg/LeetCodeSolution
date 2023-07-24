d2dcfcf81b6a393069d2467b1aa24fd654b44a9f
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-07-24 21:41:03 +0530

    Create all_possible_full_binary_trees.cpp

---
+++ 0894 All Possible Full Binary Trees/all_possible_full_binary_trees.cpp
@@ -0,0 +1,55 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int, vector<TreeNode*>> memo;
+    vector<TreeNode*> allPossibleFBT(int n) {
+        if ((n % 2) == 0) return {};
+        if (n == 1)  return {new TreeNode()};
+        if(memo.find(n) != memo.end()) return memo[n];
+
+        vector<TreeNode*> res;
+        for (int i = 1; i < n; i += 2) {
+            vector<TreeNode*> left = allPossibleFBT(i);
+            vector<TreeNode*> right = allPossibleFBT(n - i - 1);
+            for (auto l : left) {
+                for (auto r : right) {
+                    TreeNode* root = new TreeNode(0, l, r);
+                    res.push_back(root);
+                }
+            }
+        }
+        return memo[n] = res;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
