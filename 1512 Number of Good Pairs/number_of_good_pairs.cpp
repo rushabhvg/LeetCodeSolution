fd71e2280d8e519888f8a9ef56d74e89fa2fba68
RUSHABH GALA <88271018+rushabhvg@users.noreply.github.com>
2023-10-03 06:35:06 +0530

    Create number_of_good_pairs.cpp

---
+++ 1512 Number of Good Pairs/number_of_good_pairs.cpp
@@ -0,0 +1,34 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int ans = 0;
+        for(int j=1; j<nums.size(); j++) {
+            for(int i=0; i<j; i++) {
+                if (nums[i] == nums[j]) ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
