ae9dbc9350d942446c0c549cd24616121f6292b6
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-04 09:55:41 +0530

    Create 0074_search_a_2d_matrix.cpp

---
+++ 0074 Search a 2D Matrix/0074_search_a_2d_matrix.cpp
@@ -0,0 +1,40 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int low = 0;
+        int high = (rows*cols)-1;
+        while( low!=high ) {
+            int mid = low + (high-low)/2;
+            if(matrix[mid/cols][mid%cols]<target) {
+                low = mid+1;
+            } else {
+                high = mid;
+            }
+        }
+        return matrix[high/cols][high%cols] == target;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
