dbeead4ca1292ba082f0605beeef031599ba2124
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-05 09:19:52 +0530

    Create 0015_3sum.cpp

---
+++ 0015_3Sum/0015_3sum.cpp
@@ -0,0 +1,54 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size(); i++) {
+            int target = -nums[i];
+            int front = i+1;
+            int back = nums.size()-1;
+            while(front<back) {
+                int sum = nums[front] + nums[back];
+                if(sum<target) front++;
+                else if(sum>target) back--;
+                else {
+                    vector<int> triplet = {nums[i], nums[front], nums[back]};
+                    ans.push_back(triplet);
+                    while(front<back && nums[front]==triplet[1]) front++;
+                    while(front<back && nums[back]==triplet[2]) back--;
+                }
+            }
+            while(i+1<nums.size() && nums[i+1]==nums[i]) i++;
+        }
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
