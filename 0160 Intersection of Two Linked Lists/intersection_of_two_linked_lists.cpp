3694fbc3f5b0a4cef7acf76e664e6fc85a3f6e07
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-06 13:25:50 +0530

    Create intersection_of_two_linked_lists.cpp

---
+++ 0160 Intersection of Two Linked Lists/intersection_of_two_linked_lists.cpp
@@ -0,0 +1,53 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *arrA = headA;
+        ListNode *arrB = headB;
+        if(arrA==NULL || arrB==NULL) return NULL;
+        while(arrA!=NULL && arrB!=NULL && arrA!=arrB) {
+            arrA = arrA->next;
+            arrB = arrB->next;
+            if(arrA == arrB) return arrA;
+            if(arrA==NULL) arrA = headB;
+            if(arrB==NULL) arrB = headA;
+        }
+        return arrA;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
