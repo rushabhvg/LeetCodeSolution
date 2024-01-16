6d04ae07f5b466603c357c3051f45f8e80485322
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-16 22:22:58 +0530

    Create 0239_sliding_window_maximum.cpp

---
+++ 0239 Sliding Window Maximum/0239_sliding_window_maximum.cpp
@@ -0,0 +1,43 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int> dq;
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++) {
+            if(!dq.empty() && dq.front() == i-k) dq.pop_front();
+            while(!dq.empty() && nums[dq.back()]<nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if(i>=k-1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
