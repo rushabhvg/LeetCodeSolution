3e7cdd5e434399f0fe5519f3d8effae76b522e16
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-16 22:21:44 +0530

    Create 0155_min_stack.cpp

---
+++ 0155 Min Stack/0155_min_stack.cpp
@@ -0,0 +1,63 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class MinStack {
+    stack<int> s1, s2;
+public:
+    MinStack() {
+        // DN constructor
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        if(s2.empty() || x<=getMin()) s2.push(x);
+    }
+    
+    void pop() {
+        if(s1.top() == getMin()) s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
