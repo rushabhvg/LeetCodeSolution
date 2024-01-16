7c26df7d644f033d348c18eefdaf52f30d8b85d4
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-16 22:20:12 +0530

    Create 0901_online_stock_span.cpp

---
+++ 0901 Online Stock Span/0901_online_stock_span.cpp
@@ -0,0 +1,52 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class StockSpanner {
+    stack<pair<int, int>> st;
+public:
+    StockSpanner() {
+        // DNC;
+    }
+    
+    int next(int price) {
+        int res = 1;
+        while(st.size() && st.top().first <= price) {
+            res += st.top().second;
+            st.pop();
+        }
+        st.push({price, res});
+        return res;
+    }
+};
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
+
+ /*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
