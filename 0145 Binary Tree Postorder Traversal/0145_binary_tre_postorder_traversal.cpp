2b4c0fefac3b09b997c192ef57e017ac6ce1ccbd
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-17 22:11:33 +0530

    Create 0145_binary_tre_postorder_traversal.cpp

---
+++ 0145 Binary Tree Postorder Traversal/0145_binary_tre_postorder_traversal.cpp
@@ -0,0 +1,51 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+vector<int> ans;
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        if(!root) return ans;
+        ans = postorderTraversal(root->left);
+        ans = postorderTraversal(root->right);
+        ans.push_back(root->val);
+        return ans;
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
