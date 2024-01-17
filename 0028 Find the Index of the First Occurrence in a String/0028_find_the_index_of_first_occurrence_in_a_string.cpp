5fdc4c6bc9b93b883345aded3638e135ebb27c1c
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-01-17 22:10:45 +0530

    Create 0028_find_the_index_of_first_occurrence_in_a_string.cpp

---
+++ 0028 Find the Index of the First Occurrence in a String/0028_find_the_index_of_first_occurrence_in_a_string.cpp
@@ -0,0 +1,24 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        
+    }
+};
