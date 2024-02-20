822eeda4f84f3b676466055f2cd417d668d95a2e
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-02-20 06:55:51 +0530

    Create 0231_power_of_two.cpp

---
+++ 0231 Power of Two/0231_power_of_two.cpp
@@ -0,0 +1,35 @@
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+static const int _ = [](){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        return n>0 && not(n&(n-1));
+    }
+};
+
+/*
+BY RUSHABH GALA
+rushabhvg
+GitHub :
+https://github.com/rushabhvg
+StackOverflow :
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
