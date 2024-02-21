3d250c7e76c401c241381511930106e242087c2d
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-02-21 18:08:20 +0530

    Time: 1 ms (78.36%) | Memory: 44.8 MB (47.59%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,31 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int i = 1, j = 1;
+        for( ; i<nums.length; i++) 
+            if(nums[i] != nums[i-1])
+                nums[j++] = nums[i];
+        return j;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
