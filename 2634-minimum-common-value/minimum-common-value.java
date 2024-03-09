9f5795eb0b7f765a37ec6c4ef370abbfbe152bf6
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-03-09 12:48:05 +0530

    Time: 1 ms (96.67%) | Memory: 62.4 MB (80.15%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -13,26 +13,13 @@ class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
         int i = 0;
         int j = 0;
-        while(i<nums1.length && j<nums2.length) {
-            if(i<nums1.length && j<nums2.length && nums1[i]==nums2[j]) {
-                return nums1[i];
-            }
-
-            while(i<nums1.length && j<nums2.length && nums1[i]<nums2[j]) {
-                i++;
-            }
-            
-            if(i<nums1.length && j<nums2.length && nums1[i]==nums2[j]) {
-                return nums1[i];
-            }
-
-            while(i<nums1.length && j<nums2.length && nums1[i]>nums2[j]) {
-                j++;
-            }
-            
-            if(i<nums1.length && j<nums2.length && nums1[i]==nums2[j]) {
-                return nums1[i];
-            }
+        int n = nums1.length;
+        int m = nums2.length;
+        if(nums1[n-1]<nums2[0] || nums2[m-1]<nums1[0]) return -1;
+        while(i<n && j<m) {
+            if(nums1[i]==nums2[j]) return nums1[i];
+            while(i<n && j<m && nums1[i]<nums2[j]) i++;
+            while(i<n && j<m && nums1[i]>nums2[j]) j++;
         }
         return -1;
     }
