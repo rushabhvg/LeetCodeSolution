c301dbdbc42ba9bb2df48aa8c4d92454d7b7b4c4
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-03-16 11:10:56 +0530

    Time: 7 ms (98.69%) | Memory: 55.1 MB (42.69%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -16,7 +16,8 @@ class Solution {
         int curr = N;
         int res = 0;
         for(int i=0; i<N; i++) {
-            curr += (nums[i]<<1) - 1;
+            // curr += (nums[i]<<1) - 1;
+            curr = curr + (nums[i]==1 ? 1 : -1);
             if(curr == N) {
                 res = i+1;
             } else if(mp[curr]==0) {
