08728e7e9f729f70e1d24d0a7fcd3dd226e0fd3a
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-03-17 09:25:14 +0530

    Time: 1 ms (98.87%) | Memory: 44.7 MB (59.12%) - LeetSync

--- 57-insert-interval/insert-interval.java
+++ 57-insert-interval/insert-interval.java
@@ -11,33 +11,23 @@ https://www.linkedin.com/in/rushabhvg/
 
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        List<int[]> ans = new ArrayList<>();
-        int st = newInterval[0];
-        int end = newInterval[1];
-        boolean insrt = false;
-        for(int[] intvls : intervals) {
-            int cst = intvls[0];
-            int cend = intvls[1];
-            if(cend<st || insrt) {
-                ans.add(new int[]{cst, cend});
-                continue;
-            }
-            st = Math.min(st, cst);
-            if(end<cst) {
-                ans.add(new int[]{st, end});
-                ans.add(new int[]{cst, cend});
-                insrt = true;
-                continue;
-            }
-            if(end<=cend) {
-                ans.add(new int[]{st, cend});
-                insrt = true;
-            }
+        List<int[]> res = new ArrayList<>();
+        int i = 0;
+        while(i<intervals.length && intervals[i][1]<newInterval[0]) {
+            res.add(intervals[i]);
+            i++;
         }
-        if(!insrt) {
-            ans.add(new int[]{st, end});
+        while(i<intervals.length && intervals[i][0]<=newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        return ans.toArray(new int[ans.size()][]);
+        res.add(newInterval);
+        while(i<intervals.length) {
+            res.add(intervals[i]);
+            i++;
+        }
+        return res.toArray(new int[res.size()][]);
     }
 }
 
