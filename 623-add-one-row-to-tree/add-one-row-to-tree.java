9b0f1c860c38dbdf9ca7f71115ecd2a48b87cca8
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-04-16 07:12:24 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (9.38%) - LeetSync

---
+++ 623-add-one-row-to-tree/add-one-row-to-tree.java
@@ -0,0 +1,54 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private TreeNode helper(TreeNode root, int depth, int d, int val, boolean left, boolean right) {
+        if(d==depth) {
+            TreeNode node = new TreeNode(val);
+            if(left) node.left = root;
+            else node.right = root;
+            return node;
+        }
+        if(root==null) return null;
+        root.left = helper(root.left, depth, d+1, val, true, false);
+        root.right = helper(root.right, depth, d+1, val, false, true);
+        return root;
+    }
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        return helper(root, depth, 1, val, true, false);
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
