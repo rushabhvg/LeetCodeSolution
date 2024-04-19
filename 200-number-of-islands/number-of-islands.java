63ef2a9f88ca6af9f4516cf1be937b3d0a940e0a
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-04-19 22:52:43 +0530

    Time: 3 ms (86.63%) | Memory: 49.5 MB (48.61%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.java
@@ -0,0 +1,45 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public void dfs(char[][] grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!='1') return ;
+        grid[i][j] = '0';
+        dfs(grid, i+1, j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i, j-1);
+    }
+    public int numIslands(char[][] grid) {
+        if(grid==null || grid.length==0) return 0;
+        int nis = 0;
+        for(int i=0; i<grid.length; i++) {
+            for(int j=0; j<grid[0].length; j++) {
+                if(grid[i][j]=='1') {
+                    nis++;
+                    dfs(grid, i, j);
+                }
+            }
+        }
+        return nis;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
