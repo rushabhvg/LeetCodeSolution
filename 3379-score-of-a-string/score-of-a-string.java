d90adf5ed89f8748355671ed228be8eff7f1992c
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-01 09:57:54 +0530

    Time: 1 ms (99.87%) | Memory: 42 MB (41.24%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -0,0 +1,31 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int scoreOfString(String s) {
+        int sum = 0;
+        for(int i=1; i<s.length(); i++) {
+            sum += Math.abs(s.charAt(i) - s.charAt(i-1));
+        }
+        return sum;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
