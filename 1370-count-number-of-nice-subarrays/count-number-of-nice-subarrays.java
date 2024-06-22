3abbff8599415500ed22c6dad0d288efadde5390
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-22 08:00:29 +0100

    Time: 3 ms (100.00%) | Memory: 51.1 MB (99.85%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -0,0 +1,38 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int numberOfSubarrays(int[] nums, int k) {
+        int n = nums.length;
+        int[] cnt = new int[n+1];
+        cnt[0] = 1;
+        int ans = 0, t = 0;
+        for(int v:nums) {
+            t += v&1;
+            if(t-k >=0) {
+                ans += cnt[t-k];
+            }
+            cnt[t]++;
+        }
+        return ans;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
