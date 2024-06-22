8fd0c1bdeb1f8f8353de7db02cb43c1d17410dd4
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-22 18:28:44 +0100

    Time: 0 ms (100.00%) | Memory: 41.7 MB (100.00%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
@@ -0,0 +1,33 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++) {
+            if(nums[i]%3!=0) ans++;
+        }
+        return ans;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
