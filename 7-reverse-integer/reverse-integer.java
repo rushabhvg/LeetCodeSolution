6ec6aaa265cc4dd0e9255973d1022c4c3c7701f9
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-22 08:35:48 +0100

    Time: 0 ms (100.00%) | Memory: 40.5 MB (86.37%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -11,25 +11,16 @@ https://www.linkedin.com/in/rushabhvg/
 
 class Solution {
     public int reverse(int x) {
-        long num = 0;
-        int sg = (x<0) ? 1 : 0;
-        if(sg==1) x *= -1;
-        // System.out.println(x);
-        while(x>0) {
-            int rem = x%10;
-            num *= 10;
-            num += rem;
+        int rev = 0;
+        int ld = 0;
+        while(x!=0) {
+            int num = x%10;
+            rev = ld*10+num;
+            if( (rev-num)/10 != ld ) return 0;
             x /= 10;
+            ld = rev;
         }
-        if(sg==1) num *= -1;
-        // System.out.println(num);
-        if(
-            (num>=-1*(Math.pow(2,31)) )
-            && (num<=Math.pow(2, 31)-1)
-        ) {
-            return (int)num;
-        }
-        return 0;
+        return rev;
     }
 }
 
