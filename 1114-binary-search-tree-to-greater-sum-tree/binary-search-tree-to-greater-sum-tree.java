983485fb6b45c6ed69f3d70aed55bcba5b7c9958
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-25 12:50:11 +0100

    Time: 0 ms (100.00%) | Memory: 40.7 MB (93.86%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,52 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int tot = 0;
+    public void trav(TreeNode root) {
+        if(root==null) return ;
+        trav(root.right);
+        tot += root.val;
+        root.val = tot;
+        trav(root.left);
+        return ;
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        trav(root);
+        return root;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
