814dda2e1e9a6134858e1e02c535742f04c0d565
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-06-25 15:22:40 +0100

    Time: 1 ms (99.91%) | Memory: 46.2 MB (18.05%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,31 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int i=0; i<nums.length; i++) {
+            xor ^= nums[i];
+        }
+        return xor;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
