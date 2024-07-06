8ddb78281138723666b31a48c4b223e23d98750d
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-07-06 10:43:17 +0530

    Time: 19 ms (63.14%) | Memory: 58.1 MB (45.21%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -10,51 +10,30 @@ https://www.linkedin.com/in/rushabhvg/
 */
 
 class Solution {
-    public int minDays(int[] A, int m, int k) {
-        int n = A.length, left = 1, right = (int)1e9;
-        if ((long)m * k > n) return -1;
-        while (left < right) {
-            int mid = (left + right) / 2, flow = 0, bouq = 0;
-            for (int j = 0; j < n; ++j) {
-                if (A[j] > mid) {
-                    flow = 0;
-                } else if (++flow >= k) {
-                    bouq++;
-                    flow = 0;
+    public int minDays(int[] bloomDay, int m, int k) {
+        int n = bloomDay.length;
+        if((long)m*k>n) return -1;
+        int st = 1;
+        int end = (int)1e9;
+        int mid =1;
+        while(st<end) {
+            mid = (st+end)/2;
+            int currFlowers =0, bouquetsMade = 0;
+            for(int i=0; i<n; ++i) {
+                if(bloomDay[i]>mid) currFlowers=0;
+                else if(++currFlowers >= k) {
+                    bouquetsMade++;
+                    currFlowers=0;
                 }
             }
-            if (bouq < m) {
-                left = mid + 1;
-            } else {
-                right = mid;
+            if(bouquetsMade<m) {
+                st = mid+1;
+            } else{
+                end = mid;
             }
         }
-        return left;
+        return st;
     }
-    // public int minDays(int[] bloomDay, int m, int k) {
-    //     int n = bloomDay.length;
-    //     if(m*k>n) return -1;
-    //     int st = 1;
-    //     int end = (int)1e9;
-    //     int mid =1;
-    //     while(st<end) {
-    //         mid = (st+end)/2;
-    //         int currFlowers =0, bouquetsMade = 0;
-    //         for(int i=0; i<n; ++i) {
-    //             if(bloomDay[i]>mid) currFlowers=0;
-    //             else if(++currFlowers >= k) {
-    //                 bouquetsMade++;
-    //                 currFlowers=0;
-    //             }
-    //         }
-    //         if(bouquetsMade<m) {
-    //             st = mid+1;
-    //         } else{
-    //             end = mid;
-    //         }
-    //     }
-    //     return st;
-    // }
 }
 
 /*
