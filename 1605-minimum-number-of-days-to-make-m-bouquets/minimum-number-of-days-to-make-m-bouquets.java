e29e67099d5e8e6d38cf6a4cc04e3c9b9a93f201
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-07-06 10:41:25 +0530

    Time: 19 ms (63.14%) | Memory: 56 MB (86.50%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -10,41 +10,51 @@ https://www.linkedin.com/in/rushabhvg/
 */
 
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        if((long)m*k > n) {
-            return -1;
-        }
-        int low = 1, high = (int) 1e9;
-        while(low<high) {
-            int mid = low + (high-low)/2;
-            if(isPoss(bloomDay, m, k, mid)) {
-                high = mid;
+    public int minDays(int[] A, int m, int k) {
+        int n = A.length, left = 1, right = (int)1e9;
+        if ((long)m * k > n) return -1;
+        while (left < right) {
+            int mid = (left + right) / 2, flow = 0, bouq = 0;
+            for (int j = 0; j < n; ++j) {
+                if (A[j] > mid) {
+                    flow = 0;
+                } else if (++flow >= k) {
+                    bouq++;
+                    flow = 0;
+                }
+            }
+            if (bouq < m) {
+                left = mid + 1;
             } else {
-                low = mid+1;
-            }
-        };
-        return low;
-    }
-    private boolean isPoss(int[] bloomDay, int m, int k, int day) {
-        int tot = 0;
-        int n = bloomDay.length;
-        for(int i=0; i<n; i++) {
-            int cnt = 0;
-            while(i<n && cnt<k && bloomDay[i]<=day) {
-                cnt++;
-                i++;
-            }
-            if(cnt==k) {
-                tot++;
-                i--;
-            }
-            if(tot>=m) {
-                return true;
+                right = mid;
             }
         }
-        return false;
+        return left;
     }
+    // public int minDays(int[] bloomDay, int m, int k) {
+    //     int n = bloomDay.length;
+    //     if(m*k>n) return -1;
+    //     int st = 1;
+    //     int end = (int)1e9;
+    //     int mid =1;
+    //     while(st<end) {
+    //         mid = (st+end)/2;
+    //         int currFlowers =0, bouquetsMade = 0;
+    //         for(int i=0; i<n; ++i) {
+    //             if(bloomDay[i]>mid) currFlowers=0;
+    //             else if(++currFlowers >= k) {
+    //                 bouquetsMade++;
+    //                 currFlowers=0;
+    //             }
+    //         }
+    //         if(bouquetsMade<m) {
+    //             st = mid+1;
+    //         } else{
+    //             end = mid;
+    //         }
+    //     }
+    //     return st;
+    // }
 }
 
 /*
