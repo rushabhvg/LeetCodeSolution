80729c7e641c912fdeb15dd9099d5769d570e1f4
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-07-07 09:27:17 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (82.69%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -11,20 +11,7 @@ https://www.linkedin.com/in/rushabhvg/
 
 class Solution {
     public int numWaterBottles(int numBottles, int numExchange) {
-        int prevMod = 0;
-        int prevDiv = 0;
-        int ans = numBottles;
-        int mod = numBottles % numExchange;
-        int div = numBottles / numExchange;
-        ans += div;
-        while(div!=0) {
-            prevMod = mod;
-            prevDiv = div+prevMod;
-            mod = prevDiv % numExchange;
-            div = prevDiv / numExchange;
-            ans += div;
-        }
-        return ans;
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 }
 
