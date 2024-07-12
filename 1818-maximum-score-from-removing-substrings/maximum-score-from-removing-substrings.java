d8a4a02b66231c385fc7236336f2f5578f76edc9
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2024-07-12 18:11:55 +0530

    Time: 22 ms (96.97%) | Memory: 45.1 MB (96.97%) - LeetSync

--- 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
+++ 1818-maximum-score-from-removing-substrings/maximum-score-from-removing-substrings.java
@@ -11,29 +11,32 @@ https://www.linkedin.com/in/rushabhvg/
 
 class Solution {
     public int maximumGain(String s, int x, int y) {
-        int ans = 0;
-        int cntA = 0, cntB = 0;
         int n = s.length();
-        int mx = Math.min(x, y);
+        int minMul = Math.min(x, y);
+        int cntA = 0, cntB = 0;
+        int ans = 0;
         for(int i=0; i<n; i++) {
-            if((s.charAt(i)>'b') ) {
-                int mn = Math.min(cntA, cntB);
-                ans += (mn*mx);
+            if(s.charAt(i)>'b') {
+                ans += ( Math.min(cntA, cntB) * minMul);
                 cntA = 0;
                 cntB = 0;
-            } else if(s.charAt(i)=='a') {
-                if(x<y && cntB>0) {
+            } else if (s.charAt(i)=='a') {
+                if(y>x && cntB>0) {
                     cntB--;
                     ans += y;
-                } else cntA++;
-            } else if(s.charAt(i)=='b') {
+                } else {
+                    cntA++;
+                }
+            } else {
                 if(x>y && cntA>0) {
                     cntA--;
                     ans += x;
-                } else cntB++;
+                } else {
+                    cntB++;
+                }
             }
         }
-        ans += Math.min(cntA, cntB) * mx;
+        ans += ( Math.min(cntA, cntB) * minMul);
         return ans;
     }
 }
