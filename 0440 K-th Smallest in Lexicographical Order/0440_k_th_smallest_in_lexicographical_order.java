6052832827e29b81592d6515ecff32765368daad
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2025-06-09 21:58:08 +0530

    Create 0440_k_th_smallest_in_lexicographical_order.java

---
+++ 0440 K-th Smallest in Lexicographical Order/0440_k_th_smallest_in_lexicographical_order.java
@@ -0,0 +1,48 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int findKthNumber(int n, int k) {
+        int curr = 1;
+        k = k - 1;
+        while (k > 0) {
+            int steps = calcSteps(n, curr, curr+1);
+            if(steps <= k) {
+                curr += 1;
+                k -= steps;
+            } else {
+                curr *= 10;
+                k-=1;
+            }
+        }
+        return curr;
+    }
+    public int calcSteps(int n, long n1, long n2) {
+        int steps = 0;
+        while(n1 <= n) {
+            steps += Math.min(n + 1, n2) - n1;
+            n1 *= 10;
+            n2 *= 10;
+        }
+        return steps;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
