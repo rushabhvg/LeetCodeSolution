3bafaef66fbbfdf5021b56da2d7cdc28c50de4f1
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2025-07-10 22:09:04 +0530

    Create solution.java

---
+++ 3440 Reschedule Meetings for Maximum Free Time II/solution.java
@@ -0,0 +1,45 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int maxFreeTime(int eventTime, int[] startTime, int[] endTime) {
+        int[] gap = new int[startTime.length + 1], largestRight = new int[startTime.length + 1];
+        gap[0] = startTime[0];
+        for(int i = 1; i < startTime.length; i++) {
+            gap[i] = startTime[i] - endTime[i-1];
+        }
+        gap[startTime.length] = eventTime - endTime[endTime.length-1];
+        for(int i = gap.length-2; i >= 0; i--) {
+            largestRight[i] = Math.max(largestRight[i+1], gap[i+1]);
+        }
+        int ans = 0, largestLeft = 0;
+        for(int i = 1; i < gap.length; i++) {
+            int currGap = endTime[i-1] - startTime[i-1];
+            if(largestLeft >= currGap || largestRight[i] >= currGap) {
+                ans = Math.max(ans, gap[i-1] + gap[i] + currGap);
+            }
+            ans = Math.max(ans, gap[i - 1] + gap[i]);
+            largestLeft = Math.max(largestLeft, gap[i-1]);
+        }
+        return ans;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
