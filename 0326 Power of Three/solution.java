d93a91c02cd717bc97a044db013e3d2eace747f7
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2025-08-13 22:47:41 +0530

    Create solution.java

---
+++ 0326 Power of Three/solution.java
@@ -0,0 +1,33 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if( n <= 0 ) {
+            return false;
+        }
+        while( n%3 == 0) {
+            n /= 3;
+        }
+        return n==1;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
