d8bf86839f46ea4b3ae66f05ea2bb12928ae6508
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2025-08-21 22:55:53 +0530

    Create solution.java

---
+++ 1504 Count Submatrices With All Ones/solution.java
@@ -0,0 +1,43 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+    public int numSubmat(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] h = new int[n];
+        int cnt = 0;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                h[j] = mat[i][j] == 1 ? h[j] + 1 : 0;
+            }
+            for(int j = 0; j < n; j++) {
+                int mn = h[j];
+                for(int k = j; k >= 0 && mn > 0; k--) {
+                    mn = Math.min(mn, h[k]);
+                    cnt += mn;
+                }
+            }
+        }
+        return cnt;
+    }
+}
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
