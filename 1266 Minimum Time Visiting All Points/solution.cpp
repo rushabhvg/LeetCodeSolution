52dc1a2498b0433ce14397e8a68b75e2c120837b
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2026-01-12 22:29:47 +0530

    Add solution for minimum time visiting all points
    Implement solution to calculate minimum time to visit all points.

---
+++ 1266 Minimum Time Visiting All Points/solution.cpp
@@ -0,0 +1,32 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        for (int i = 1; i < points.size(); i++) {
+            ans += max(abs(points[i][0] - points[i - 1][0]), abs(points[i][1] - points[i - 1][1]));
+        }
+        return ans;
+    }
+};
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
