5e57894a5f12f6475c574d29b03eb38bdadeaa5a
Rushabh Gala <88271018+rushabhvg@users.noreply.github.com>
2026-02-18 22:01:29 +0530

    Add solution for checking alternating bits in binary
    Implement a function to check if a number has alternating bits.

---
+++ 0693 Binary Number with Alternating Bits/solution.cpp
@@ -0,0 +1,33 @@
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
+
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+        int a = n & 1;
+        while ((n & 1) == a) {
+            a = 1 - a;
+            n >>= 1;
+        }
+        return n == 0;
+    }
+};
+
+/*
+By Rushabh Gala
+rushabhvg
+GitHub:
+https://github.com/rushabhvg
+StackOverflow:
+https://stackoverflow.com/users/16571212/rushabhvg
+LinkedIn:
+https://www.linkedin.com/in/rushabhvg/
+*/
